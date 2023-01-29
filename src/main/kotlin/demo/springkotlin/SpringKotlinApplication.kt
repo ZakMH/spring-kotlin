28188d0d7aec0af7bc7358f883d4a9c526bc5ed9
M'harek Zakaria <zmharek@sqli.com>
2023-01-29 06:31:27 +0100

    init spring-kotlin demo project

---
+++ src/main/kotlin/demo/springkotlin/SpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package demo.springkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinApplication>(*args)
+}
